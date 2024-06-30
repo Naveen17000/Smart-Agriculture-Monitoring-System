c9444a7b29c4e21e703abae346c8f4f250377973
Naveen17000 <167189786+Naveen17000@users.noreply.github.com>
2024-06-30 13:10:47 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,16 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import tensorflow as tf
 from tensorflow.keras.losses import BinaryCrossentropy
-from tensorflow.keras.layers import Dense,Input
+from tensorflow.keras.layers import Dense
 from tensorflow.keras import Sequential
-from tensorflow.keras.activations import relu,sigmoid
 from tensorflow.keras.optimizers import Adam
-
 data = pd.read_csv("TARP.csv")
 
 soil_moisture = data["Soil Moisture"]
@@ -31,13 +26,13 @@ scaler = StandardScaler()
 X_norm = scaler.fit_transform(X_train)
 X_test = scaler.fit_transform(X_test)
 model = Sequential()
-model.add(Dense(units = 100,activation = "relu"))
-model.add(Dense(units = 50,activation ="relu"))
+model.add(Dense(units = 10,activation = "relu"))
+model.add(Dense(units = 5,activation ="relu"))
 model.add(Dense(units =1,activation ="sigmoid"))
 model.compile(loss = BinaryCrossentropy(), optimizer = Adam())
 model.fit(X_norm,y_train,epochs = 100)
 y_hat = model.predict(X_test)
 y_pred_discrete = (y_hat > 0.5).astype(int)
+
 model.save('my_model.h5')  # Save the model
-accuracy = accuracy_score(y_test, y_pred_discrete)
-print(accuracy)
+
